7319aea79c0db9e8f7408b5467ef6a805b6238d8
gasgit <g00305555@gmit.ie>
2017-01-03 22:30:18 +0000

    create objects

---
+++ create_obj.rb
@@ -0,0 +1,53 @@
+!#/usr/bin/ruby
+print "Create Objects Ruby \n"
+
+# format 1 create object and methods
+hello = Object.new
+def hello.talk
+    puts"Hello from format 1"
+end
+
+def hello.laugh
+    puts"Ha ha from format 1"
+end
+print "#{hello.talk}"
+print "#{hello.laugh}"
+
+# format 2 create class
+class Hello
+    def talk
+        puts"I am a talking object!!"
+    end
+    def laugh
+        puts"Ha Ha Ha"
+    end
+end
+
+obj = Hello.new
+obj.talk
+obj.laugh
+
+
+#format 3 
+class Student
+  @@student_count=0
+  def initialize(id,first,last)
+    @id=id
+    @first=first
+    @last=last
+  end
+  def display_student()
+    puts "Student ID: #@id"
+    puts "First Name; #@first"
+    puts "Last Name: #@last"
+  end
+  def num_students()
+    @@student_count +=1
+    puts "Total Students: #@@student_count"
+  end
+
+end
+
+st1=Student.new("1","ozzy","osbourne")
+st1.display_student()
+st1.num_students()

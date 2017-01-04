411316ac689b062d6ebb48364df0729272dff41f
gasgit <g00305555@gmit.ie>
2017-01-04 15:58:33 +0000

    update range

--- range.rb
+++ range.rb
@@ -4,6 +4,10 @@
   print n," "
 end
 print "\n"
+# output
+# 6 7 8 9 10 11 12 13 14 15 16
+#
+
 # seperator in new array
 $, =", "
 #range nums to array
@@ -13,5 +17,15 @@ range2 = ('a'..'z').to_a
 # range appending to end of string
 range3 = ('saa'..'saz').to_a
 puts "#{range1}"
+# output
+#[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+#
 puts "#{range2}"
+# output
+# ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+#
 puts "#{range3}"
+# output
+# ["saa", "sab", "sac", "sad", "sae", "saf", "sag", "sah", "sai", "saj", "sak", "sal", "sam", "san", "sao", "sap", "saq", "sar", "sas", "sa
+# t", "sau", "sav", "saw", "sax", "say", "saz"]
+#

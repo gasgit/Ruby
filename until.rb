6203d4511fd9efbf35960ef8a86367a3aa5d9116
gasgit <g00305555@gmit.ie>
2017-01-04 15:53:34 +0000

    test

--- until.rb
+++ until.rb
@@ -10,11 +10,26 @@ until $i > $num do
   $i +=1
 end
 
+#.output
+# Value until statment: 0
+# Value until statment: 1
+# Value until statment: 2
+# Value until statment: 3
+# Value until statment: 4
+# Value until statment: 5
+
 # until modifier
 $i = 0
 $num = 5
-
 begin
   puts "Value until modifier: #{$i}"
   $i +=1
 end until $i > $num
+
+# output
+# Value until modifier: 0
+# Value until modifier: 1
+# Value until modifier: 2
+# Value until modifier: 3
+# Value until modifier: 4
+# Value until modifier: 5

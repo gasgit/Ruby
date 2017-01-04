5942004bd319d721f4c01e90e58393b2c9cb60c7
gasgit <g00305555@gmit.ie>
2017-01-04 00:12:03 +0000

    for variations

---
+++ for.rb
@@ -0,0 +1,27 @@
+!#/usr/bin/ruby
+
+# normal for loop
+for i in 0..10
+  puts "Value is: #{i}"
+end
+
+# break in loop
+for i in 0..5
+  if i > 2 then
+    break
+  end
+  puts "Value break condition: #{i}"
+end
+
+# next in loop
+for i in 10..20
+  if i < 15 then
+    next
+  end
+  puts "Value next condition: #{i}"
+end
+
+# for in loop
+(0..5).each do |i|
+  puts "Value for each: #{i}"
+end

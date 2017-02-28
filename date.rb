0ebfcd8b6bc918e5763a4260a112cb2b952a9a48
gasgit <g00305555@gmit.ie>
2017-02-28 22:13:56 +0000

    time

---
+++ date.rb
@@ -0,0 +1,17 @@
+#!/usr/bin/ruby 
+
+
+time = Time.new
+
+# year, month, day, hour, min, sec
+puts"Time now: " + time.inspect
+
+# day number of week
+puts time.wday
+# day number of year
+puts time.yday
+# hour of day
+puts time.hour
+#mint of hour
+puts time.min
+
